f843867118f081c3d6286eade96891fbb89855c3
Preston Kwei <prestonkwei@gmail.com>
2024-12-15 09:51:45 -0800

    more info page almost

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ console.log(id)
 
 document.querySelector('#clientid-fill').innerHTML = id
 
+document.querySelector('#more').addEventListener('click', () => {
+  if (document.querySelector('#info-more').classList.contains('hidden')) {
+    document.querySelector('#more').innerHTML = 'Less Info'
+  } else {
+    document.querySelector('#more').innerHTML = 'More Info'
+  }
+  document.querySelector('#info-more').classList.toggle('hidden')
+})
 function captchaSucess() {
   console.log('captchacomplete')
 }
