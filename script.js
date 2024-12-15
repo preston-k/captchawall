884e85601f832122388f3013bcd3b70608b5aa38
Preston Kwei <prestonkwei@gmail.com>
2024-12-15 10:07:24 -0800

    stop logging

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if (window.location.href.contains('.com')) {
+  console.log = function () {}
+}
+
 let id = crypto.randomUUID()
 
 console.log(id)
@@ -11,6 +15,14 @@ document.querySelector('#more').addEventListener('click', () => {
     document.querySelector('#more').innerHTML = 'More Info'
   }
   document.querySelector('#info-more').classList.toggle('hidden')
+})
+function bgScore() {
+  console.log('v3 Run')
+}
+grecaptcha.ready(() => {
+  grecaptcha.execute('6Lc-hZwqAAAAAIG6M1p_Wwrs83Fgtja9aci8JZrS', { action: 'homepage' }).then((token) => {
+    console.log('reCAPTCHA token:', token)
+  })
 })
 function captchaSucess() {
   console.log('captchacomplete')
